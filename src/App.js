e5d139d48ec2cec584235e6c2793c2e0969b5be0
Barış Erzen <122172611+bariserzen@users.noreply.github.com>
2023-06-08 18:33:38 +0300

    accordion-dropdown

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Accordion from './components/Accordion';
+
+
+
+
+const  App = () => {
+
+
+  return <Accordion/>;
+};
+
+export default App;
